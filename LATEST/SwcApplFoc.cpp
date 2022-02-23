5d048cceb62d210896a50e12efc80f371f4c1adf
root <nagaraja.hm.0001@gmail.com>
2022-02-23 18:27:08 +0100

    Update: submodule SwcApplFoc

--- LATEST/SwcApplFoc.cpp
+++ LATEST/SwcApplFoc.cpp
@@ -1,11 +1,15 @@
 /*****************************************************/
-/* File   : Swc_Foc.cpp                              */
+/* File   : SwcApplFoc.cpp                           */
 /* Author : Naagraaj HM                              */
 /*****************************************************/
 
 /*****************************************************/
 /* #INCLUDES                                         */
 /*****************************************************/
+#include "module.h"
+#include "SwcApplFoc_EcuM.h"
+#include "SwcApplFoc_SchM.h"
+#include "SwcApplFoc_Unused.h"
 
 /*****************************************************/
 /* #DEFINES                                          */
@@ -18,6 +22,16 @@
 /*****************************************************/
 /* TYPEDEFS                                          */
 /*****************************************************/
+class module_SwcApplFoc:
+      public abstract_module
+   ,  public interface_SwcApplFoc_EcuM
+   ,  public interface_SwcApplFoc_SchM
+{
+   public:
+      FUNC(void, SWCAPPLFOC_CODE) InitFunction   (void);
+      FUNC(void, SWCAPPLFOC_CODE) DeInitFunction (void);
+      FUNC(void, SWCAPPLFOC_CODE) MainFunction   (void);
+};
 
 /*****************************************************/
 /* CONSTS                                            */
@@ -30,10 +44,22 @@
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
+module_SwcApplFoc SwcApplFoc;
+
+interface_SwcApplFoc_EcuM *EcuM_Client_ptr_SwcApplFoc = &SwcApplFoc;
+interface_SwcApplFoc_SchM *SchM_Client_ptr_SwcApplFoc = &SwcApplFoc;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
+FUNC(void, SWCAPPLFOC_CODE) InitFunction(void){
+}
+
+FUNC(void, SWCAPPLFOC_CODE) DeInitFunction(void){
+}
+
+FUNC(void, SWCAPPLFOC_CODE) MainFunction(void){
+}
 
 /*****************************************************/
 /* EOF                                               */
