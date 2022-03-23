05e90836efc35747540079a520b73eea517529d5
root <nagaraja.hm.0001@gmail.com>
2022-03-23 13:09:37 +0100

    Update: MATLAB/Simulink application software component SwcApplFoc

--- LATEST/SwcApplFoc.cpp
+++ LATEST/SwcApplFoc.cpp
@@ -40,6 +40,16 @@ class module_SwcApplFoc:
       FUNC(void, SWCAPPLFOC_CODE) DeInitFunction (void);
       FUNC(void, SWCAPPLFOC_CODE) GetVersionInfo (void);
       FUNC(void, SWCAPPLFOC_CODE) MainFunction   (void);
+
+   private:
+      CONST(Std_TypeVersionInfo, SWCAPPLFOC_CONST) VersionInfo = {
+            0x0000
+         ,  0xFFFF
+         ,  0x01
+         ,  '0'
+         ,  '1'
+         ,  '0'
+      };
 };
 
 /******************************************************************************/
