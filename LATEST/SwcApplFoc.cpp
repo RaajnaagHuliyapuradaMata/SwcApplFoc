5cf8ab8f2388b96ef6cac685d929a9e9bfbfb7bf
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-03 10:10:04 +0000

    Update: Application software component for field oriented motor control

--- LATEST/SwcApplFoc.cpp
+++ LATEST/SwcApplFoc.cpp
@@ -82,79 +82,79 @@ FUNC(void, SWCAPPLFOC_CODE) module_SwcApplFoc::InitFunction(
    CONSTP2CONST(CfgModule_TypeAbstract, SWCAPPLFOC_CONFIG_DATA, SWCAPPLFOC_APPL_CONST) lptrCfgModule
 ){
 #if(STD_ON == SwcApplFoc_InitCheck)
-   if(E_OK == IsInitDone){
-#if(STD_ON == SwcApplFoc_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
+   if(E_OK != IsInitDone){
 #endif
-   }
-   else{
-#endif
-      if(NULL_PTR == lptrCfgModule){
-#if(STD_ON == SwcApplFoc_DevErrorDetect)
-         Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-         );
-#endif
-      }
-      else{
+      if(NULL_PTR != lptrCfgModule){
          if(STD_LOW){
-// check lptrCfgModule for memory faults
+            // check lptrCfgModule for memory faults
             lptrCfg = lptrCfgModule;
          }
          else{
-// use PBcfgCanIf as back-up configuration
+            // use PBcfgCanIf as back-up configuration
 //            lptrCfg = &PBcfgSwcApplFoc;
          }
       }
-      IsInitDone = E_OK;
+      else{
+#if(STD_ON == SwcApplFoc_DevErrorDetect)
+         Det_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
 #if(STD_ON == SwcApplFoc_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == SwcApplFoc_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
+#endif
    }
 #endif
 }
 
 FUNC(void, SWCAPPLFOC_CODE) module_SwcApplFoc::DeInitFunction(void){
 #if(STD_ON == SwcApplFoc_InitCheck)
-   if(E_OK != IsInitDone){
-#if(STD_ON == SwcApplFoc_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
+   if(E_OK == IsInitDone){
 #endif
+#if(STD_ON == SwcApplFoc_InitCheck)
+      IsInitDone = E_NOT_OK;
    }
    else{
+#if(STD_ON == SwcApplFoc_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
 #endif
-      IsInitDone = E_NOT_OK;
-#if(STD_ON == SwcApplFoc_InitCheck)
    }
 #endif
 }
 
 FUNC(void, SWCAPPLFOC_CODE) module_SwcApplFoc::MainFunction(void){
 #if(STD_ON == SwcApplFoc_InitCheck)
-   if(E_OK != IsInitDone){
-#if(STD_ON == SwcApplFoc_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
-#endif
-   }
-   else{
+   if(E_OK == IsInitDone){
 #endif
 #if(STD_ON == SwcApplFoc_InitCheck)
+   }
+   else{
+#if(STD_ON == SwcApplFoc_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
+#endif
    }
 #endif
 }
