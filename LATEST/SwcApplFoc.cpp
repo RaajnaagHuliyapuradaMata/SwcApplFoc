543b2a6740c916b0295c43699f8b23e87e323b61
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-04 09:02:30 +0000

    Update: Application software component for field oriented motor control

--- LATEST/SwcApplFoc.cpp
+++ LATEST/SwcApplFoc.cpp
@@ -113,7 +113,7 @@ FUNC(void, SWCAPPLFOC_CODE) module_SwcApplFoc::InitFunction(
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  SWCAPPLFOC_E_UNINIT
       );
 #endif
    }
@@ -133,7 +133,7 @@ FUNC(void, SWCAPPLFOC_CODE) module_SwcApplFoc::DeInitFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  SWCAPPLFOC_E_UNINIT
       );
 #endif
    }
@@ -152,7 +152,7 @@ FUNC(void, SWCAPPLFOC_CODE) module_SwcApplFoc::MainFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  SWCAPPLFOC_E_UNINIT
       );
 #endif
    }
