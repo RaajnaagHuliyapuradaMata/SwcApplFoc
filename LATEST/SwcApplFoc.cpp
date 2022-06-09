376d713a312712f7c1c9d7f61b22ae48e0f75291
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-09 14:08:00 +0000

    Update: Application software component for field oriented motor control

--- LATEST/SwcApplFoc.cpp
+++ LATEST/SwcApplFoc.cpp
@@ -74,10 +74,10 @@ CONSTP2VAR(infSchMClient, SWCAPPLFOC_VAR, SWCAPPLFOC_CONST) gptrinfSchMClient_Sw
 VAR(module_SwcApplFoc, SWCAPPLFOC_VAR) SwcApplFoc(
    {
 #if(STD_ON == _ReSIM)
-         "SwcApplFoc"
+         "SwcApplFoc",
 #else
 #endif
-      ,  SWCAPPLFOC_AR_RELEASE_VERSION_MAJOR
+         SWCAPPLFOC_AR_RELEASE_VERSION_MAJOR
       ,  SWCAPPLFOC_AR_RELEASE_VERSION_MINOR
       ,  0x00
       ,  0xFF
