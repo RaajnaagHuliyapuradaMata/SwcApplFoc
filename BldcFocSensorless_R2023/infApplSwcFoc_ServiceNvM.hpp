a855e02914b340bc9c5253374f06c4e24a9d3126
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:49:08 +0100

    Update: Application software component for field oriented motor control

---
+++ BldcFocSensorless_R2023/infApplSwcFoc_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infApplSwcFoc_ServiceNvM.hpp                                      */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTAPPLSWCFOC_AR_RELEASE_VERSION_MAJOR                               4
+#define CONSTAPPLSWCFOC_AR_RELEASE_VERSION_MINOR                               3
+
+#define Const_DefaultInit_ConstApplSwcFoc                                                                              \
+      {CONSTAPPLSWCFOC_AR_RELEASE_VERSION_MAJOR, CONSTAPPLSWCFOC_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
