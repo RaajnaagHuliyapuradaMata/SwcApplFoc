29df3d44d7660f88aabba53430da07dfe7ab834a
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-02-04 19:27:20 +0100

    Update: Application software component for field oriented motor control

--- BldcFocSensorless_R2023/infApplSwcFoc_ServiceNvM_Types.hpp
+++ BldcFocSensorless_R2023/infApplSwcFoc_ServiceNvM_Types.hpp
@@ -10,9 +10,11 @@
 class NvM_BlocksRom_ApplSwcFoc_Type:
       public ConstModule_TypeAbstract
 {
-/******************************************************************************/
-/* CONSTS                                                                     */
-/******************************************************************************/
+};
+
+class NvM_BlocksNv_ApplSwcFoc_Type:
+      public CfgModule_TypeAbstract
+{
 };
 
 /******************************************************************************/
