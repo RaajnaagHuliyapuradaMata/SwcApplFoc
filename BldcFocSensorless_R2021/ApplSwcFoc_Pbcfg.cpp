cf0f82f293339317264e9129bcff3def5689a4a2
Raajnaag HULIYAPURADA MATA <nagaraja.hm.0001@gmail.com>
2024-06-12 20:03:14 +0530

    Initial commit

--- BldcFocSensorless_R2021/ApplSwcFoc_Pbcfg.cpp
+++ BldcFocSensorless_R2021/ApplSwcFoc_Pbcfg.cpp
@@ -1,23 +1,7 @@
 /******************************************************************************/
 /* File   : Template.hpp                                                      */
-/*                                                                            */
-/* Author : Nagaraja HULIYAPURADA MATA                                        */
-/*                                                                            */
-/* License / Warranty / Terms and Conditions                                  */
-/*                                                                            */
-/* Everyone is permitted to copy and distribute verbatim copies of this lice- */
-/* nse document, but changing it is not allowed. This is a free, copyright l- */
-/* icense for software and other kinds of works. By contrast, this license is */
-/* intended to guarantee your freedom to share and change all versions of a   */
-/* program, to make sure it remains free software for all its users. You have */
-/* certain responsibilities, if you distribute copies of the software, or if  */
-/* you modify it: responsibilities to respect the freedom of others.          */
-/*                                                                            */
-/* All rights reserved. Copyright © 1982 Nagaraja HULIYAPURADA MATA           */
-/*                                                                            */
-/* Always refer latest software version from:                                 */
-/* https://github.com/NagarajaHuliyapuradaMata?tab=repositories               */
-/*                                                                            */
+/* Author : Nagaraja HULIYAPURADA-MATA                                        */
+/* Date   : 01.02.1982                                                        */
 /******************************************************************************/
 
 /******************************************************************************/
